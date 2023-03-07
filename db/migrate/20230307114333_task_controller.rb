a744e669429ade61803ca949acc6ca4ba594b045
Leonardo Genzolini <leonardogenzolini@gmail.com>
2023-03-07 12:46:08 +0100

    add TaskController

---
+++ db/migrate/20230307114333_task_controller.rb
@@ -0,0 +1,4 @@
+class TaskController < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_112609) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_114333) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
